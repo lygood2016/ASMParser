3c7dced85309491c862227e1da520e4ddb5f76ae
Li <amznyl@amazon.com>
2016-11-22 11:16:34 -0800

    Initial Commit

---
+++ ASMParser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ASMParser
+//
+//  Created by Li, Yan on 11/22/16.
+//  Copyright © 2016 Li, Yan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
